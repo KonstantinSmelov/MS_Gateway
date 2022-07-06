a899e09aa29936276325f0140df0de3d87eed93d
Konstantin <kostya_smelov@inbox.ru>
2022-07-06 14:53:30 +0300

    develop init

--- src/main/java/neostudy/feignclient/DealClient.java
+++ src/main/java/neostudy/feignclient/DealClient.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "deal-service-client", url = "${services.deal.url}")
 public interface DealClient {
 
-    @GetMapping("/deal/application/{applicationId}")
-    SummaryAppInfo getSummaryAppInfoFromDeal(@PathVariable Long applicationId);
+//    @GetMapping("/deal/application/{applicationId}")
+//    SummaryAppInfo getSummaryAppInfoFromDeal(@PathVariable Long applicationId);
 }
